5979623b318a4f80a784000f7991526874d14548
arhan0514 <34431819+arhan0514@users.noreply.github.com>
2023-11-21 16:03:51 +0900

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import  { NextAuthOptions } from “next-auth”; //added
+import {NextAuthOptions} from 'next-auth';
 import NextAuth, {AuthOptions} from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from "next-auth/providers/github";
